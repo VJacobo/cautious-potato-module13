5fc01eaffc8cad5c2457a5f17f14de82ae01597c
VJacobo <victoriajacobo99@gmail.com>
2023-12-14 19:01:41 -0600

    Fixed the router.get in the product-routes.js file.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,46 +1,50 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('./models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/' , async (req, res) => {
   try {
     const products = await Product.findAll({
+      include: [
+         { model: Category },
+         { model: Tag, through: ProductTag },
+      ],
+    });
+
+    res.json(products);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: ' Internal Server Error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
       include: [
         { model: Category },
         { model: Tag, through: ProductTag },
       ],
     });
-  res.json(products);
-} catch (err) {
-  console.error(err);
-  res.status(500).json({ message: 'Internal Server Error'});
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found'});
+    }
+    res.json(product);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: ' Internal Server Error'});
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+module.exports = router;
+  // find all products
   // be sure to include its associated Category and Tag data
-try {
-  const product = await Product.findByPk(req.params.id, {
-    include: [
-      {model: Category },
-      { model: Tag, through: ProductTag },
-    ],
-  });
-  if (!product) {
-    return res.status(404).json({ message: 'Product not found'});
-  }
-  res.json(product);
-} catch (err) {
-  console.error(err);
-  res.status(500).json({ message: 'Internal Server Error'});
-}
-});
+
+  // be sure to include its associated Category and Tag data
+
 
 // create new product
 router.post('/', (req, res) => {
